17bf2833de4248e7895bb431cdc9cfcd1aa3d0e5
Erik Svensson <eosvensson@gmail.com>
2021-11-08 08:40:35 +0100

    Initialized project

---
+++ src/main/java/se/lexicon/g39_functional_interfaces/App.java
@@ -0,0 +1,13 @@
+package se.lexicon.g39_functional_interfaces;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
